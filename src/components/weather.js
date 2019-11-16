32501e22f60a230f0e97713c169d1ac52accc8bd
imhta <imhtapm@gmail.com>
2019-11-16 19:10:54 +0530

    ui version alpha completed

--- src/components/weather.js
+++ src/components/weather.js
@@ -3,13 +3,14 @@ const renderWeather = ({
   weather, wind, main, name, country,
 }) => {
   weatherElement.innerHTML = `
+    <h1 class="title">Current Weather</h1>
     <header>
       <h1>${name}, ${country} ${main.temp}<sup>o</sup><small>C</small>  ${weather[0].main}</h1>
       <img src="http://openweathermap.org/img/wn/${weather && weather[0].icon}@2x.png" alt="${weather[0].main}">
     </header>
-    <h2> wind speed: ${wind.speed}<small>m/s</small>  direction: ${wind.deg}<sup>o</sup></h2>
+    <h2> wind speed: ${wind.speed}<small>m/s</small> | direction: ${wind.deg}<sup>o</sup></h2>
     <h3>Cloudiness ${weather[0].description} </h3>
-    <p>pressure ${main.pressure} hpa Humidity ${main.humidity} %</p>  
+    <p>pressure ${main.pressure} hpa | Humidity ${main.humidity} %</p>  
   `;
 };
 
