e99043da01702828cc40904503755d6e23c4bef0
imhta <imhtapm@gmail.com>
2019-11-18 20:46:31 +0530

    basic functional output for data based on data

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,11 +1,18 @@
 const weatherElement = document.getElementById('weather');
+// (0°C × 9/5) + 32 = 32°F
+const getWeatherByUnit = (weather, unit) => {
+  if (unit === 'C') {
+    return weather;
+  }
+  return ((weather * (9 / 5)) + 32).toFixed(2);
+};
 const renderWeather = ({
   weather, wind, main, name, country,
-}) => {
+}, unit) => {
   weatherElement.innerHTML = `
     <h1 class="title">Current Weather</h1>
     <header>
-      <h1>${name}, ${country} ${main.temp}<sup>o</sup><small>C</small>  ${weather[0].main}</h1>
+      <h1>${name}, ${country} ${getWeatherByUnit(Number(main.temp), unit)}<sup>o</sup><small>${unit}</small>  ${weather[0].main}</h1>
       <img src="http://openweathermap.org/img/wn/${weather && weather[0].icon}@2x.png" alt="${weather[0].main}">
     </header>
     <h2> wind speed: ${wind.speed}<small>m/s</small> | direction: ${wind.deg}<sup>o</sup></h2>
--- src/index.js
+++ src/index.js
@@ -7,18 +7,32 @@ import renderEmpty from './components/empty';
 import './scss/main.scss';
 
 const cityInputField = document.getElementById('city-input');
+const toggleElement = document.getElementById('toggle');
+let currentData;
+let currentUnit = 'C';
 
 cityInputField.addEventListener('keyup', debounce(({ target }) => {
   if (target.value.replace(/\s/g, '').length) {
     renderLoading();
     fetchWeather(target.value).then((data) => {
       if (data.error.cod === 200) {
-        renderWeather(data);
+        currentData = data;
+        renderWeather(data, currentUnit);
       } else {
+        currentData = undefined;
         renderNotFound(target.value, data.error);
       }
     });
   } else {
+    currentData = undefined;
     renderEmpty();
   }
 }, 300));
+
+toggleElement.addEventListener('click', ({ target }) => {
+  // eslint-disable-next-line no-console
+  currentUnit = target.id;
+  if (currentData) {
+    renderWeather(currentData, currentUnit);
+  }
+});
